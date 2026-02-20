aa36daf0dd08708db50974894dd44f30791d8282
HebaRabaya <hebraba55001ds@gmail.com>
2026-02-20 02:04:23 +0200

    Add test for employee addition using Playwright
    This test automates the process of adding a new employee, including logging in and filling out the employee details.

--- test/test_add_employee.py
+++ test/test_add_employee.py
@@ -1,78 +1,23 @@
-Record
+from playwright.sync_api import sync_playwright
 
+def test_add_employee():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
 
+        # صفحة تسجيل الدخول
+        page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        page.fill('input[name="username"]', "Admin")
+        page.fill('input[name="password"]', "admin123")
+        page.click('button[type="submit"]')
 
+        # الانتقال إلى Add Employee
+        page.click("text=PIM")
+        page.click("text=Add Employee")
 
+        # ملأ بيانات الموظف الجديد
+        page.fill('input[name="firstName"]', "Test")
+        page.fill('input[name="lastName"]', "User")
+        page.click('button[type="submit"]')
 
-
-
-
-
-Target:
-
-Library
-
-
-25
-    page2 = context.new_page()
-26
-    page2.goto("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList")
-27
-    page2.close()
-28
-    page1.get_by_role("textbox", name="Type for hints...").first.click()
-29
-    page1.get_by_role("textbox", name="Type for hints...").first.press("ControlOrMeta+ر")
-30
-    page1.get_by_role("textbox", name="Type for hints...").first.click()
-31
-    page1.get_by_role("textbox", name="Type for hints...").first.fill("Test")
-32
-    page1.get_by_role("textbox").nth(2).click()
-33
-    page1.get_by_role("button", name=" Add").click()
-34
-    page1.get_by_role("button", name="Reload").click()
-35
-    page1.get_by_role("textbox", name="Username").click()
-36
-    page1.get_by_role("textbox", name="Username").fill("Admin")
-37
-    page1.locator("div").filter(has_text=re.compile(r"^Password$")).nth(1).click()
-38
-    page1.get_by_role("textbox", name="Password").fill("admin123")
-39
-    page1.get_by_role("textbox", name="Password").press("Enter")
-40
-    page1.get_by_role("button", name="Login").click()
-41
-    page1.get_by_role("textbox", name="First Name").click()
-42
-    page1.get_by_role("textbox", name="First Name").fill("Test")
-43
-    page1.get_by_role("textbox", name="Last Name").click()
-44
-    page1.get_by_role("textbox", name="Last Name").fill("User")
-45
-    page1.get_by_role("button", name="Save").click()
-46
-    page1.goto("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/167")
-47
-​
-48
-    # ---------------------
-49
-    context.close()
-50
-    browser.close()
-51
-​
-52
-​
-53
-with sync_playwright() as playwright:
-54
-    run(playwright)
-55
-​
-
+        browser.close()
